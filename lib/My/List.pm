5865dc914e46b8cb2d0dfbff52ab31cd5d8dd988
Tomoki Aonuma <uasi@99cm.org>
2011-08-18 18:31:09 +0900

    Add a bunch of methods

--- lib/My/List.pm
+++ lib/My/List.pm
@@ -11,23 +11,41 @@ sub new {
     my $head = My::List::Cell->new();
     bless {
         head => $head,
-        tail => $head
+        tail => $head,
+        size => 0
     }, $class;
 }
 
 sub append {
     my ($self, $value) = @_;
     $self->{tail} = My::List::Cell->new($self->{tail}, $value);
+    $self->{size}++;
 }
 
 sub clear {
     my ($self) = @_;
     $self->{head} = $self->{tail} = My::List::Cell->new();
+    $self->{size} = 0;
+}
+
+sub size {
+    my ($self) = @_;
+    $self->{size};
 }
 
 sub iterator {
     my ($self) = @_;
-    My::List::Iterator->new($self->{head});
+    My::List::Iterator->new($self);
+}
+
+sub to_array {
+    my ($self) = @_;
+    my @array;
+    my $iter = $self->iterator;
+    while ($iter->has_next) {
+        push @array, $iter->next->value;
+    }
+    @array;
 }
 
 1;
--- lib/My/List/Iterator.pm
+++ lib/My/List/Iterator.pm
@@ -5,8 +5,11 @@ use warnings;
 use Carp;
 
 sub new {
-    my ($class, $cell) = @_;
-    bless {cell => $cell}, $class;
+    my ($class, $list) = @_;
+    bless {
+        list => $list,
+        cell => $list->{head}
+    }, $class;
 }
 
 sub has_next {
@@ -18,6 +21,15 @@ sub next {
     my ($self) = @_;
     $self->{cell} = $self->{cell}->{next};
     $self->{cell};
+}
+
+sub remove {
+    my ($self) = @_;
+    unless (defined $self->{cell}->{prev}) {
+        croak "Head cannot be removed";
+    }
+    $self->{cell}->{prev} = $self->{cell}->{next};
+    $self->{list}->{size}--;
 }
 
 1;
